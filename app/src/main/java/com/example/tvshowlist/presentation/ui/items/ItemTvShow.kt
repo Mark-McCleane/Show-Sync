40eb9c7496360e4d291d9875d5531a189f747eca
Mark McCleane <mccleanemark@gmail.com>
2025-04-03 17:36:58 +0100

    added dropdown for ItemTvShow.kt

--- app/src/main/java/com/example/tvshowlist/presentation/ui/items/ItemTvShow.kt
+++ app/src/main/java/com/example/tvshowlist/presentation/ui/items/ItemTvShow.kt
@@ -1,24 +1,33 @@
 package com.example.tvshowlist.presentation.ui.items
 
+import android.R
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -27,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.tvshowlist.domain.model.TvShow
@@ -42,7 +52,7 @@ fun ItemTvShow(
     navigateTo: (tvShow: TvShow) -> Unit
 ) {
     var isExpanded by remember { mutableStateOf(false) }
-    val context = LocalContext.current
+
     Card(
         modifier = modifier
             .fillMaxWidth()
@@ -57,14 +67,15 @@ fun ItemTvShow(
                 AsyncImage(
                     model = imageUrl,
                     contentDescription = "$tvShowTitle Cover Image",
-                    error = painterResource(id = android.R.drawable.stat_notify_error),
+                    error = painterResource(id = R.drawable.stat_notify_error),
                     contentScale = ContentScale.Fit,
                     modifier = Modifier.size(110.dp, 110.dp)
                 )
             }
             Spacer(modifier = Modifier.width(8.dp))
+            val clickableModifier = Modifier.clickable { isExpanded = !isExpanded }
             Column(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(0.75f)
             ) {
                 Text(
                     text = tvShowTitle,
@@ -73,22 +84,49 @@ fun ItemTvShow(
                     textAlign = TextAlign.Center
                 )
                 Divider(thickness = 2.dp)
-                val clickableModifier = Modifier.clickable { isExpanded = !isExpanded }
                 Text(
                     text = tvShowTagLine.ifEmpty { "No description found" },
                     overflow = TextOverflow.Ellipsis,
                     maxLines = if (isExpanded) Int.MAX_VALUE else 2,
-                    modifier = clickableModifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(
                     text = "Aired: $tvShowAirDate",
-                    modifier = clickableModifier
+                    modifier = modifier
                         .fillMaxWidth()
                         .wrapContentHeight()
                 )
             }
-
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = clickableModifier
+                    .weight(1f)
+                    .defaultMinSize(minHeight = 110.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = if (isExpanded) painterResource(android.R.drawable.arrow_up_float)
+                    else painterResource(android.R.drawable.arrow_down_float),
+                    contentDescription = null,
+                    modifier = clickableModifier)
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewItemTvShow() {
+    val tvShow =
+        TvShow(0, "This is a very looooooooong Test Description", "Test Title", "2022-02-20")
+    ItemTvShow(
+        tvShow = tvShow,
+        tvShowTitle = tvShow.title,
+        tvShowAirDate = tvShow.airDate ?: "",
+        tvShowTagLine = tvShow.description ?: "",
+        imageUrl = "",
+        navigateTo = {}
+    )
 }
