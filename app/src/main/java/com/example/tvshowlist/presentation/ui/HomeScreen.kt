39e795fae5e0c98e28c3956fcc3055bc959ffb4e
Mark McCleane <mccleanemark@gmail.com>
2025-05-13 22:20:25 +0100

    added logo on home screen scaffold

--- app/src/main/java/com/example/tvshowlist/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/tvshowlist/presentation/ui/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.tvshowlist.presentation.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -32,8 +35,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -79,7 +84,21 @@ fun HomeScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "TV Show App") },
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            imageVector = ImageVector.vectorResource(R.drawable.show_sync_backgroundless_logo),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(size = 50.dp)
+                        )
+                        Text(
+                            text = stringResource(R.string.app_name)
+                        )
+                    }
+                },
                 actions = {
                     IconButton(onClick = { navigateToSettings() }) {
                         Icon(
@@ -133,7 +152,7 @@ fun HomeScreen(
                 if (searchText.isEmpty() && recentTvShowList.isEmpty()) {
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                         Text(
-                            text = "Please Search For A Tv Show",
+                            text = stringResource(R.string.please_search_for_a_tv_show),
                             style = TextStyle(fontSize = 50.sp, fontWeight = FontWeight.ExtraBold),
                             textAlign = TextAlign.Center,
                         )
